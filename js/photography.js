7a83a0596909721875b4ef5d4539bd8ee24b571a
Aaditya Vikram <aadityavikram628@gmail.com>
2026-03-28 02:18:10 +0530

    Added a card for sections in photography and added a button to go back to Top

--- js/photography.js
+++ js/photography.js
@@ -29,3 +29,17 @@ document.addEventListener('keydown', (e) => {
         lightbox.style.display = 'none';
     }
 });
+
+const topBtn = document.getElementById('topBtn');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      topBtn.style.display = 'block';
+    } else {
+      topBtn.style.display = 'none';
+    }
+  });
+
+  topBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
