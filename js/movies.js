9f9186889dabb3b8a7cec1c3bcd65e578c932b0e
Aaditya Vikram <aadityavikram628@gmail.com>
2026-04-02 10:53:41 +0530

    Added movies to JSON

--- js/movies.js
+++ js/movies.js
@@ -17,7 +17,7 @@ let startX = 0;
 let currentX = 0;
 let isDragging = false;
 const moviesUrl = ''
-const moviesJson = 'https://ik.imagekit.io/aadivik/Me/json/movies_9OClsh5Zo.json'
+const moviesJson = 'https://ik.imagekit.io/aadivik/Me/json/movies_oj5IBP-QM.json'
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
